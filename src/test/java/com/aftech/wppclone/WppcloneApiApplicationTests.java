f09507f9ad5b819f98202472f325103bc126f29e
Alexandre <alex.silva250@hotmail.com>
2025-07-15 23:56:16 -0300

    feat: initial configurations in the front-end application

---
+++ src/test/java/com/aftech/wppclone/WppcloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aftech.wppclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WppcloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
